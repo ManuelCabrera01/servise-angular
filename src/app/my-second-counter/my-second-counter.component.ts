1cf968a0b5e6c4daf7cab317a77c6530a256095b
Manuel <m.a.c.p90@gmail.com>
2017-11-11 11:45:33 -0500

    error fixed

--- src/app/my-second-counter/my-second-counter.component.ts
+++ src/app/my-second-counter/my-second-counter.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import { CounterService } from '../service/counter.service';
 @Component({
   selector: 'app-my-second-counter',
   templateUrl: './my-second-counter.component.html',
   styleUrls: ['./my-second-counter.component.css'],
-  encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.None
+  providers: [CounterService]
 })
 export class MySecondCounterComponent implements OnInit {
-
-  constructor() { }
+  constructor(private theCounter: CounterService) {}
 
   ngOnInit() {
   }
